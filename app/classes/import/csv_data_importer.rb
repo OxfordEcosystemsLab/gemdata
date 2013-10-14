0de8b2822c24deecbdc4cc15511bb6d830937e47
J Potts <jopotts@gmail.com>
2013-10-14 11:50:06 +0100

    More feedback on import process

--- app/classes/import/csv_data_importer.rb
+++ app/classes/import/csv_data_importer.rb
@@ -2,76 +2,94 @@ require 'csv'
 
 class CSVDataImporter
 
-  attr_reader :errors
+  def initialize(ar_class, csv_file_name)
+    @ar_class = ar_class
+    @csv_file_name = csv_file_name
+  end
 
-  def import_all
-    message "Importing..."
+  def import!
 
     # IngrowthCoreValues.delete_all # TEMP TEMP TEMP TEMP TEMP
 
-    import_data("ICEltr_sample.csv", IngrowthCoreValues)
-    # Add more
+    import_data
 
-    message "Done."
-    message "ERRORS:\n#{@errors.join("\n")}" unless @errors.blank?
   end
 
   private
 
-    def import_data(csv_file, ar_class)
-      class_name = ar_class.to_s.underscore.gsub('_',' ')
-      message(" - #{class_name}")
-      full_path = Rails.root.join('db', 'import', csv_file)
+    def import_data
+      class_name = @ar_class.to_s.underscore.gsub('_',' ').capitalize
+      put_message "Importing #{class_name}...", time: true
+
+      failed, updated, inserted, ignored = 0, 0, 0, 0
+
       begin
+        @ar_class.transaction do
 
-        # Get a hash of the unique keys to id
-        current_keys = ar_class.get_unique_keys_hash
+          # Get a hash of all current unique keys to id
+          current_keys = @ar_class.get_unique_keys_hash
 
-        # Loop the CSV rows
-        ar_class.transaction do
+          # Loop the CSV rows
+          full_path = Rails.root.join('db', 'import', @csv_file_name)
           CSV.foreach(full_path, headers: true) do |row|
 
             # Swap NaN values for blanks
             row_hash = row.to_hash
             row_hash.map { |k,v| row_hash[k] = (v == 'NaN' ? '' : v) }
 
+            # Jump to next row if blank
+            if row_hash.blank?
+              put_message " Row #{$.} failed - empty row"
+              next
+            end
+
             # Create the AR object and validate
-            record = ar_class.new(row_hash)
-            if record.valid?
+            new_record = @ar_class.new(row_hash)
+            if new_record.valid?
 
               # Check for an existing record
-              existing_record_id = current_keys[record.unique_key]
+              existing_record_id = current_keys[new_record.unique_key]
 
               # Save or update
               if existing_record_id
-                ar_class.find_by(id: existing_record_id).update_attributes(row_hash)
+                existing = @ar_class.find_by(id: existing_record_id)
+                existing.attributes = row_hash
+                existing.changed? ? updated += 1 : ignored += 1
+                existing.save
               else
-                record.save
+                new_record.save
+                inserted += 1
               end
 
             else
-
-              # Log the error
-              all_errors = record.errors.messages.map { |k,v| "#{k} #{v.first}" }.join(", ")
-              add_error("Row #{$.} - #{all_errors}")
-
+              # Record not valid: log the error
+              all_errors = new_record.errors.messages.map { |k,v| "#{k} #{v.first}" }.join(", ")
+              put_message " Row #{$.} failed - #{all_errors}"
+              failed += 1
             end
 
           end
         end
+
+        # Completion feedback
+        result = []
+        result << "Import complete."
+        result << "Processed: #{inserted + updated + ignored + failed} (#{inserted} created, #{updated} updated, #{ignored} not changed, #{failed} failed)."
+        result << "Table total rows: #{@ar_class.count}"
+        put_message result.join(" "), time: true
+
       rescue Exception => ex
-        add_error("Aborted! #{class_name} - #{ex.message}")
+        put_message "Import aborted! - #{ex.message}", time: true
       end
     end
 
-    def message(text)
+    def put_message(text, options={})
+      text = "[#{timestamp}] #{text}" if options[:time]
       puts(text)
-      # Rails.logger.debug(text)
     end
 
-    def add_error(message)
-      @errors ||= []
-      @errors << message
+    def timestamp
+      Time.now.strftime("%H:%M:%S")
     end
 
 end
--- lib/tasks/import.rake
+++ lib/tasks/import.rake
@@ -3,8 +3,8 @@ namespace :import do
   desc "Import csv data into csv holding tables"
 
   task csv_data: :environment do
-    csv_importer = CSVDataImporter.new
-    csv_importer.import_all
+    csv_importer = CSVDataImporter.new(IngrowthCoreValues, "ICEltr_sample.csv")
+    csv_importer.import!
   end
 
 end
