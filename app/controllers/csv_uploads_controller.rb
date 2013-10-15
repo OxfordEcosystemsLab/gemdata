b5007278c2d56a5909195710dda4633590e11248
J Potts <jopotts@gmail.com>
2013-10-15 17:05:58 +0100

    Check mime type

--- app/controllers/csv_uploads_controller.rb
+++ app/controllers/csv_uploads_controller.rb
@@ -17,15 +17,21 @@ class CsvUploadsController < ApplicationController
 
   def create
     if params[:file]
-      import_id = SecureRandom.hex
-      import_log = ImportLog.new(import_id)
-      ar_class = params[:csv_class].constantize
-      csv_importer = CSVDataImporter.new(ar_class, params[:file].tempfile, import_log)
-      Thread.new do
-        csv_importer.import!
+      if params[:file].content_type == "text/csv"
+        import_id = SecureRandom.hex
+        import_log = ImportLog.new(import_id)
+        ar_class = params[:csv_class].constantize
+        csv_importer = CSVDataImporter.new(ar_class, params[:file].tempfile, import_log)
+        Thread.new do
+          csv_importer.import!
+        end
+        redirect_to csv_upload_path(id: import_id)
+      else
+        flash[:notice] = "This file does not appear to be a valid csv file. It has a content type of: #{params[:file].content_type}"
+        redirect_to new_csv_upload_path
       end
-      redirect_to csv_upload_path(id: import_id)
     else
+      flash[:notice] = "Please select a csv file."
       redirect_to new_csv_upload_path
     end
   end
