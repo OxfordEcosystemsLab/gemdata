d02db068f59bc65a0604586efc1344374d8c6374
cecilegirardin <cecilegirardin@gmail.com>
2013-10-16 16:22:05 +0100

    deleted tree_height, added dbh_year and dbh_growth_cm.
    dbh_year: year of first dbh measurement
    dbh_growth_cm: dbh (measured in cm) + growth (measured in mm)

--- app/models/dendrometer_values.rb
+++ app/models/dendrometer_values.rb
@@ -7,10 +7,11 @@ class DendrometerValues < ActiveRecord::Base
 
   validates :sub_plot,      allow_nil: true, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
 
-  validates :tree_height_m, allow_nil: true, numericality: { greater_than_or_equal_to: 0.5, less_than_or_equal_to: 150 }
   validates :dbh_height_m,  allow_nil: true, numericality: { greater_than_or_equal_to: 0.2, less_than_or_equal_to: 10 }
   validates :dbh_cm,        allow_nil: true, numericality: { greater_than_or_equal_to: 1,   less_than_or_equal_to: 500 }
-
+  validates :dbh_date,      allow_nil: true, numericality: { greater_than_or_equal_to: 1983, less_than_or_equal_to: 2050 }
+  validates :dbh_growth_cm, allow_nil: true, numericality: { greater_than_or_equal_to: 5, less_than_or_equal_to: 1500 }
+  
   def self.unique_key_columns
     [:plot_code, :year, :month, :day, :sub_plot, :tree_tag]
   end
