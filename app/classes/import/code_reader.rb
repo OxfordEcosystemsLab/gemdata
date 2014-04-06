065b2a66224797151d938640cf135029bfa4cf12
Ben Holmes <bdvholmes@gmail.com>
2014-04-06 14:24:23 +0100

    Move code logic out to a code reader class
    This class knows a lot about plot, tree and branch. As the codes start to
    become more complicated I will move the logic for branch codes to Branch
    for example.

---
+++ app/classes/import/code_reader.rb
@@ -0,0 +1,24 @@
+class CodeReader
+
+  attr_reader :plot_code, :tree_code, :branch_code, :suffix
+
+
+  def initialize(code)
+    format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))-(\w+\d*\w*)-?(.*)$/
+    match = code.match(format) 
+    if (match)
+      @plot_code   = match[1]
+      @tree_code   = match[2]
+      @branch_code = match[3]
+      @suffix      = match[4]
+    else
+      raise("Could not extract branch code for #{code}")
+    end
+  end
+
+  def find_or_create_branch
+    plot   = Plot.where(:plot_code => plot_code).first_or_create
+    tree   = TraitsTree.where(:code => tree_code, :plot_id => plot.id).first_or_create
+    branch = Branch.where(:code => branch_code, :traits_tree_id => tree.id).first_or_create
+  end
+end
