51cd8ba20f02be887f39001f99a4c6c3d20b2042
Ben Holmes <bdvholmes@gmail.com>
2014-04-06 14:24:41 +0100

    Update toughness measurement to use new code reader

--- app/models/toughness_measurement.rb
+++ app/models/toughness_measurement.rb
@@ -12,14 +12,12 @@ class ToughnessMeasurement < ActiveRecord::Base
   validates :punch,       presence: true
 
   def code=(code)
-    branch_code = extract_branch_code(code)
-    tree_code   = extract_tree_code(code)
-    plot_code   = extract_plot_code(code)
+    reader = CodeReader.new(code)
 
-    plot   = Plot.where(:plot_code => plot_code).first_or_create
-    tree   = TraitsTree.where(:code => tree_code, :plot_id => plot.id).first_or_create
-    branch = Branch.where(:code => branch_code, :traits_tree_id => tree.id).first_or_create
+    branch = reader.find_or_create_branch
     write_attribute(:branch_id, branch.id)
+
+    @leaf_code = reader.suffix
   end
 
   def read_csv(line)
@@ -34,29 +32,5 @@ class ToughnessMeasurement < ActiveRecord::Base
     write_attribute(:lines, values[7])
     write_attribute(:punch, values[8])
   end
-
-  private
-
-    def extract_plot_code(code)
-      extract_match(code, 1)
-    end
-
-    def extract_tree_code(code)
-      extract_match(code, 2)
-    end
-
-    def extract_branch_code(code)
-      extract_match(code, 3)
-    end
-
-    def extract_match(code, i)
-      code_format = /^(WAY\d+)-((?:[IT]|CSP)\d+)-(B?\d.*)$/
-      @match ||= code.match(code_format)
-      if (@match)
-        return @match[i]
-      else
-        raise("Could not extract branch code for #{code}")
-      end
-    end
 
 end
--- spec/models/toughness_measurement_spec.rb
+++ spec/models/toughness_measurement_spec.rb
@@ -2,37 +2,8 @@ require "spec_helper"
 
 describe ToughnessMeasurement do
 
-  before :each do
-    @valid = ToughnessMeasurement.new
-    @valid.date      = Date.new
-    @valid.evaluator = 'Ben'
-    @valid.replica   = 'L24'
-    @valid.thickness = 12.1
-    @valid.width     = 12.2
-    @valid.lines     = 42
-    @valid.punch     = 2.1
-  end
-
   it 'is not valid on its own' do
     expect(ToughnessMeasurement.new).to_not be_valid
-  end
-
-  it 'can read a T code' do
-    t = @valid
-    t.code = "WAY01-T1031-B1S"
-    expect(t).to be_valid
-  end
-
-  it 'can read a CSP code' do
-    t = @valid
-    t.code = "WAY01-CSP28003-77-S"
-    expect(t).to be_valid
-  end
-
-  it 'raises an exception on invalid code' do
-    t = @valid
-    expect{t.code = "What's all this? What's all this?"}.to raise_error
-    expect(t).to_not be_valid
   end
 
   it 'can read CSV' do
