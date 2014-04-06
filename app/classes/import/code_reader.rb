e6a0b7c1f8aa0e028a16d941abfc4ca713a30599
Ben Holmes <bdvholmes@gmail.com>
2014-04-06 20:55:24 +0100

    Tidy up find or create branch code

--- app/classes/import/code_reader.rb
+++ app/classes/import/code_reader.rb
@@ -2,7 +2,6 @@ class CodeReader
 
   attr_reader :plot_code, :tree_code, :branch_code, :suffix
 
-
   def initialize(code)
     format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))-(\w+\d*\w*)-?(.*)$/
     match = code.match(format) 
@@ -17,8 +16,8 @@ class CodeReader
   end
 
   def find_or_create_branch
-    plot   = Plot.where(:plot_code => plot_code).first_or_create
-    tree   = TraitsTree.where(:code => tree_code, :plot_id => plot.id).first_or_create
-    branch = Branch.where(:code => branch_code, :traits_tree_id => tree.id).first_or_create
+    plot = Plot.where(:plot_code => plot_code).first_or_create
+    tree = TraitsTree.where(:code => tree_code, :plot_id => plot.id).first_or_create
+    Branch.where(:code => branch_code, :traits_tree_id => tree.id).first_or_create
   end
 end
