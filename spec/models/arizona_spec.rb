7b9a5187fcbee338f2ead88ada3d7a5b7f731f06
Ben Holmes <bdvholmes@gmail.com>
2014-04-07 23:17:56 +0100

    Write a spec for the arizona model
    Currently pending tests

--- spec/models/arizona_spec.rb
+++ spec/models/arizona_spec.rb
@@ -1,5 +1,35 @@
 require 'spec_helper'
 
 describe Arizona do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @az = Arizona.new
+    @az.leaf = Leaf.create(:code => 'L1L')
+    @az.date = Date.new(2014,4,5)
+    @az.evaluators = 'Me-Myself-Irene'
+    @az.fresh_mass = 2.31
+    @az.dry_mass   = 2.21
+    @az.thickness  = 5.32
+  end
+
+  it 'is not valid on its own' do
+    expect(Arizona.new).to_not be_valid
+  end
+
+  it 'can be valid' do
+    expect(@az).to be_valid
+  end
+
+  it 'should check that dry mass is less than fresh mass'
+
+  it 'should check that numbers are positive'
+
+  it 'should not let a P code not have petiole width'
+  it 'should not let a P code have thickness'
+
+  it 'should not let an L code not have thickness'
+  it 'should not let an L code have petiole width'
+
+  it 'should also let an T code have petiole width'
+
 end
