8034643c6a5557a4821cb2e8bf3ffd7647b495d1
Ben Holmes <bdvholmes@gmail.com>
2014-04-08 20:12:21 +0100

    Define test and validation for leaf

--- app/models/leaf.rb
+++ app/models/leaf.rb
@@ -1,3 +1,6 @@
 class Leaf < ActiveRecord::Base
   belongs_to :branch
+
+  validates :code, :presence => true
+  validates :branch, :presence => true
 end
--- spec/models/leaf_spec.rb
+++ spec/models/leaf_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe Leaf do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    plot = Plot.new(:plot_code => 'abc')
+    tree = TraitsTree.new(:code => 'def', :plot => plot)
+    branch = Branch.new(:code => 'ghi', :traits_tree => tree)
+    @leaf = Leaf.new(:code => 'jkl', :branch => branch)
+  end
+
+  it 'can be valid' do
+    expect(@leaf).to be_valid
+  end
+
+  it 'must have a branch' do
+    @leaf.branch = nil
+    expect(@leaf).to_not be_valid
+    expect(@leaf).to have(1).errors_on(:branch)
+  end
+
+  it 'must have a code' do
+    @leaf.code = nil
+    expect(@leaf).to_not be_valid
+    expect(@leaf).to have(1).errors_on(:code)
+  end
 end
