a17a626fba36952fc4b045cf4f109dc23bd12b00
Ben Holmes <bdvholmes@gmail.com>
2014-04-21 18:02:56 +0100

    Define import status

--- app/classes/value_objects/lookup.rb
+++ app/classes/value_objects/lookup.rb
@@ -2,6 +2,7 @@ class Lookup
   extend Lookups
 
   define_lookup "RowStatus", %w(imported updated)
+  define_lookup "ImportStatus", %w(updated skipped ignored inserted failed)
   define_lookup "QualityCode", %w(ok)
 
 end
