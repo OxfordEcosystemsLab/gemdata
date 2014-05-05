c217b04a15d8ed8861eb3d81124424953be58fb4
Ben Holmes <bdvholmes@gmail.com>
2014-05-05 23:52:36 +0100

    Generate a schema for photosynthesis

---
+++ app/models/photosynthesis_measurement.rb
@@ -0,0 +1,2 @@
+class PhotosynthesisMeasurement < ActiveRecord::Base
+end
---
+++ spec/models/photosynthesis_measurement_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe PhotosynthesisMeasurement do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
