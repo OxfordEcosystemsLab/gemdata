e6c7c5a35a395de38eefdef7123d9011847ce5b3
Ben Holmes <bdvholmes@gmail.com>
2014-06-08 21:08:21 +0100

    Tests for dbh

--- app/models/dbh_measurement.rb
+++ app/models/dbh_measurement.rb
@@ -1,4 +1,6 @@
 class DbhMeasurement < ActiveRecord::Base
   belongs_to :tree
   belongs_to :census
+
+  validates :tree, :census, :presence => true
 end
--- spec/models/dbh_measurement_spec.rb
+++ spec/models/dbh_measurement_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe DbhMeasurement do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @dbh = DbhMeasurement.new
+    @dbh.tree = Tree.new
+    @dbh.census = Census.new
+  end
+
+  it 'can be valid' do
+    expect(@dbh).to be_valid
+  end
+
+  it 'is not valid without a census' do
+    @dbh.census = nil
+    expect(@dbh).to_not be_valid
+  end
+
+  it 'is not valid without a tree' do
+    @dbh.tree = nil
+    expect(@dbh).to_not be_valid
+  end
+
 end
+
