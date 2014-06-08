6b6abd55dfe7ac8fbcd56d8c0c0531fa45d5e753
Ben Holmes <bdvholmes@gmail.com>
2014-06-08 18:17:04 +0100

    Forest plots importer selects existing plots / species complex

--- app/classes/importers/forest_plots_importer.rb
+++ app/classes/importers/forest_plots_importer.rb
@@ -7,12 +7,19 @@ class ForestPlotsImporter < BaseImporter
     @tree.fp_id = values[10]
     @tree.tree_code = 'T' + values[19]
 
-    plot = Plot.find_or_create_by!(:fp_id => values[0], :plot_code => values[1].delete('-'))
+    plot_code = strip_dashes(values[1])
+    plot = Plot.find_or_initialize_by(
+      :fp_id => values[0],
+      :plot_code => plot_code,
+    )
+    plot.plot_desc = values[2]
+    plot.save!
+
     @tree.sub_plot = SubPlot.find_or_create_by!(:plot_id => plot.id)
 
-    family = FpFamily.create(:apg_id => values[11], :name => values[12])
-    genus  = FpGenus.create(:fp_id => values[13], :name => values[14], :fp_family => family)
-    @tree.fp_species = FpSpecies.create(:fp_id => values[15], :name => values[16], :fp_genus => genus)
+    family = FpFamily.find_or_create_by!(:apg_id => values[11], :name => values[12])
+    genus  = FpGenus.find_or_create_by!(:fp_id => values[13], :name => values[14], :fp_family => family)
+    @tree.fp_species = FpSpecies.find_or_create_by!(:fp_id => values[15], :name => values[16], :fp_genus => genus)
 
     if @tree.save
       status = Lookup::ImportStatus.inserted
@@ -22,4 +29,10 @@ class ForestPlotsImporter < BaseImporter
 
     return ImportResult.new(@tree, status)
   end
+
+  private
+    def self.strip_dashes(value)
+      value.delete('-')
+    end
+
 end
--- spec/classes/importers/forest_plots_importer_spec.rb
+++ spec/classes/importers/forest_plots_importer_spec.rb
@@ -3,12 +3,15 @@ require 'csv'
 require 'importer'
 
 describe ForestPlotsImporter do
+
   it_behaves_like 'Importer'
 
-  it 'can read CSV' do
-    values = CSV.parse_line '90,TAM-04,Tambopata plot two swamp edge clay,PERU,Oliver Phillips,1983.67,1,90,Main Plot View,,54832,377,Sapotaceae,24801,Pouteria,653110,Pouteria indet,,,2,105,105,105,105,1300,a,1,5,0,,,'
+  before :each do
+    @values = CSV.parse_line '90,TAM-04,Tambopata plot two swamp edge clay,PERU,Oliver Phillips,1983.67,1,90,Main Plot View,,54832,377,Sapotaceae,24801,Pouteria,653110,Pouteria indet,,,2,105,105,105,105,1300,a,1,5,0,,,'
+  end
 
-    result = ForestPlotsImporter.read_row(values, Array.new)
+  it 'can read CSV' do
+    result = ForestPlotsImporter.read_row(@values, Array.new)
     tree = result.ar_class
     expect(result.status).to eq(Lookup::ImportStatus.inserted)
 
@@ -18,6 +21,7 @@ describe ForestPlotsImporter do
     plot = Plot.find(tree.sub_plot.plot_id)
     expect(plot.fp_id).to eq(90)
     expect(plot.plot_code).to eq('TAM04')
+    expect(plot.plot_desc).to eq('Tambopata plot two swamp edge clay')
 
     species = tree.fp_species
     expect(species.name).to eq('Pouteria indet')
@@ -34,8 +38,24 @@ describe ForestPlotsImporter do
     expect(tree).to be_valid
   end
 
-  it 'selects existing plots'
-  it 'selects existing species'
-  it 'selects existing genera'
-  it 'selects existing families'
+  it 'selects existing plots and subplots' do
+
+    plot = Plot.create!(:plot_code => 'TAM04', :fp_id => 90)
+
+    tree = ForestPlotsImporter.read_row(@values, Array.new).ar_class
+    expect(tree.sub_plot.plot).to eq(plot)
+
+  end
+
+  it 'selects existing species complex' do
+
+    fp_family = FpFamily.create!(apg_id: 377, name: 'Sapotaceae')
+    fp_genus = FpGenus.create!(fp_id: 24801, name: 'Pouteria', fp_family: fp_family)
+    fp_species = FpSpecies.create!(fp_id: 653110, name: 'Pouteria indet', fp_genus: fp_genus)
+
+    tree = ForestPlotsImporter.read_row(@values, Array.new).ar_class
+    expect(tree.fp_species).to eq(fp_species)
+
+  end
+
 end
