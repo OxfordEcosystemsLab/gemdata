d696354e29e40de559eb96f8b1787e317a3d997e
Ben Holmes <bdvholmes@gmail.com>
2014-06-09 19:42:49 +0100

    Implement and test cn_measurement

--- app/models/cn_measurement.rb
+++ app/models/cn_measurement.rb
@@ -1,4 +1,7 @@
 class CnMeasurement < ActiveRecord::Base
   belongs_to :cn_curve
   belongs_to :leaf
+
+  validates :leaf, :cn_curve, :presence => true
+  validates :n15_delta, :c13_delta, :numericality => { :less_than => 0 }
 end
--- spec/models/cn_measurement_spec.rb
+++ spec/models/cn_measurement_spec.rb
@@ -1,5 +1,42 @@
 require 'spec_helper'
 
 describe CnMeasurement do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @cn = CnMeasurement.new
+    @cn.leaf = Leaf.new(:code => 'L1L')
+    @cn.row = 'A'
+    @cn.column = 1
+    @cn.c_percent = 49.4
+    @cn.n_percent = 1.4
+    @cn.cn_ratio = 36.3
+    @cn.n15_delta = -3.3
+    @cn.c13_delta = -27.7
+    @cn.cn_curve = CnCurve.new
+  end
+
+  it 'can be valid' do
+    expect(@cn).to be_valid
+  end
+
+  it 'is invalid without a leaf' do
+    @cn.leaf = nil
+    expect(@cn).to_not be_valid
+  end
+
+  it 'is invalid without a curve' do
+    @cn.cn_curve = nil
+    expect(@cn).to_not be_valid
+  end
+
+  it 'expects a negative N15 delta' do
+    @cn.n15_delta = 4
+    expect(@cn).to_not be_valid
+  end
+
+  it 'expects a negative C13 delta' do
+    @cn.c13_delta = 4
+    expect(@cn).to_not be_valid
+  end
+
 end
