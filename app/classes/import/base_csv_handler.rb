5cc52c432b06ef50bbb031c213e3b70dbc043fca
Ben Holmes <bdvholmes@gmail.com>
2014-06-09 23:36:20 +0100

    Refactor BaseCsvHandler

--- app/classes/import/base_csv_handler.rb
+++ app/classes/import/base_csv_handler.rb
@@ -6,6 +6,7 @@ class BaseCsvHandler
     @ar_class = ar_class
     @csv_file = csv_file
     @logger = ImportLogger.new(results, time: true)
+    @status_counts = Hash.new(0)
   end
 
   # Import in another thread
@@ -24,45 +25,46 @@ class BaseCsvHandler
 
       @logger.notice "Importing #{@ar_class.table_human_name}..."
 
-      status_counts = Hash.new(0)
       @ar_class.transaction do
-
-        transaction_has_errors = false
-
-        # Loop the CSV rows
-        CSV.foreach(@csv_file, headers: true) do |row|
-          begin
-            importer = @ar_class.new
-            status = importer.read_row(row, @logger) || Lookup::ImportStatus.failed
-            status_counts[status] += 1
-          rescue => ex
-            transaction_has_errors = true
-            @logger.error "Import aborted processing line #{$.}! - #{ex.message}"
-            if ex.class.to_s == "ActiveRecord::UnknownAttributeError"
-              allowed_attribute_list = @ar_class.allowed_attributes.join(", ")
-              @logger.notice "Allowed column names: #{allowed_attribute_list}"
-            end
-          end
-        end
-
-        if transaction_has_errors
+        if !handle_csv
           raise 'transaction_has_errors'
         end
       end
 
-      # Completion feedback
-      total_rows = status_counts.length
       @logger.notice "Import complete."
+      @logger.notice generate_summary_message
+      @logger.notice "Table '#{@ar_class.table_name}' new row count: #{@ar_class.count}"
+    end
+
+    def handle_csv
+
+      transaction_is_ok = true
+
+      CSV.foreach(@csv_file, headers: true) do |row|
+        begin
+          importer = @ar_class.new
+          status = importer.read_row(row, @logger) || Lookup::ImportStatus.failed
+          @status_counts[status] += 1
+        rescue => ex
+          transaction_is_ok = false
+          @logger.error "Import aborted processing line #{$.}! - #{ex.message}"
+        end
+      end
+
+      transaction_is_ok
+    end
+
+    def generate_summary_message
+      # Completion feedback
+      total_rows = @status_counts.length
       result = []
       result << "Results: #{total_rows} rows processed"
-      result << "#{status_counts['inserted']} created"
-      result << "#{status_counts['updated']} updated"
-      result << "#{status_counts['ignore']} not changed"
-      result << "#{status_counts['skipped']} skipped"
-      result << "#{status_counts['failed']} failed"
-      @logger.notice result.join(", ")
-
-      @logger.notice "Table '#{@ar_class.table_name}' new row count: #{@ar_class.count}"
+      result << "#{@status_counts['inserted']} created"
+      result << "#{@status_counts['updated']} updated"
+      result << "#{@status_counts['ignore']} not changed"
+      result << "#{@status_counts['skipped']} skipped"
+      result << "#{@status_counts['failed']} failed"
+      result.join(", ")
     end
 
 end
