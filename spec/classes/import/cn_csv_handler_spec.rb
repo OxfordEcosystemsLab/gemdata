cc687f663eb7705d4edac229acf49970842e6287
Ben Holmes <bdvholmes@gmail.com>
2014-06-12 00:07:12 +0100

    CN handler gets a spec

---
+++ spec/classes/import/cn_csv_handler_spec.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+describe CnCsvHandler do
+
+  it 'can do an import with a cn_curve' do
+
+    plot = Plot.create!(:plot_code => 'SPD01')
+    sub_plot = SubPlot.create!(:plot_id => plot.id)
+    fp_species = FpSpecies.new
+    tree = Tree.create!(:tree_code => 'T541', :sub_plot => sub_plot, :fp_species => fp_species)
+
+    handler = CnCsvHandler.new(CnImporter, "#{Rails.root}/spec/fixtures/cn.csv", Array.new)
+    handler.import!
+
+    cn_curve = handler.cn_curve
+
+    expect(cn_curve).to_not be_nil
+
+    expect(cn_curve.c_enr_1).to eq(-29.35)
+    expect(cn_curve.c_enr_2).to eq(-29.27)
+
+    expect(cn_curve.n_enr_1).to eq(-2.16)
+    expect(cn_curve.c_percent_1).to eq(71.11)
+    expect(cn_curve.n_percent_1).to eq(10.37)
+    expect(cn_curve.cn_ratio_1).to eq(6.85)
+
+  end
+
+end
