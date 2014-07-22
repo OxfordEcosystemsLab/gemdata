6808e6f95d10a13504e685209bd508e23e23e9f9
Ben Holmes <bdvholmes@gmail.com>
2014-07-22 09:48:05 +0100

    Generate batches model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706215411) do
+ActiveRecord::Schema.define(version: 20140722083024) do
+
+  create_table "batches", force: true do |t|
+    t.string   "import_address"
+    t.datetime "started"
+    t.datetime "finished"
+    t.boolean  "transaction_passed"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "branch_architectures", force: true do |t|
     t.integer  "branch_id"
