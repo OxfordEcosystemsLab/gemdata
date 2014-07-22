1f65f7886b559b92e3274aab055beec2ff860b33
Ben Holmes <bdvholmes@gmail.com>
2014-07-22 09:48:24 +0100

    Generate batches scaffolding

---
+++ app/controllers/batches_controller.rb
@@ -0,0 +1,83 @@
+class BatchesController < ApplicationController
+  before_action :set_batch, only: [:show, :edit, :update, :destroy]
+
+  # GET /batches
+  # GET /batches.json
+  def index
+    @batches = Batch.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @batches }
+    end
+  end
+
+  # GET /batches/1
+  # GET /batches/1.json
+  def show
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @batch }
+    end
+  end
+
+  # GET /batches/new
+  def new
+    @batch = Batch.new
+  end
+
+  # GET /batches/1/edit
+  def edit
+  end
+
+  # POST /batches
+  # POST /batches.json
+  def create
+    @batch = Batch.new(batch_params)
+
+    respond_to do |format|
+      if @batch.save
+        format.html { redirect_to @batch, notice: 'Batch was successfully created.' }
+        format.json { render json: @batch, status: :created }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @batch.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /batches/1
+  # PATCH/PUT /batches/1.json
+  def update
+    respond_to do |format|
+      if @batch.update(batch_params)
+        format.html { redirect_to @batch, notice: 'Batch was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: 'edit' }
+        format.json { render json: @batch.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /batches/1
+  # DELETE /batches/1.json
+  def destroy
+    @batch.destroy
+    respond_to do |format|
+      format.html { redirect_to batches_url }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_batch
+      @batch = Batch.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def batch_params
+      params.require(:batch).permit(:import_address, :started, :finished, :transaction_passed)
+    end
+end
