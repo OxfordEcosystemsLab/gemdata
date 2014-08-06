bbbdc1b6d71a17230892d3ca1cec21fe5c62d481
Ben Holmes <bdvholmes@gmail.com>
2014-08-06 15:30:47 +0100

    Define CNP validations

--- app/models/cnp_measurement.rb
+++ app/models/cnp_measurement.rb
@@ -2,4 +2,14 @@ class CnpMeasurement < ActiveRecord::Base
   include BatchImport
 
   belongs_to :leaf
+
+  validates :leaf, :presence => true
+
+  validates :n_percent, :c_percent, :cn_ratio, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }, :allow_nil => true
+  validates :n15_delta, :n13_delta, :numericality => { :less_than => 0 }
+  validates :p_corrected_var_coef,  :numericality => { :less_than => 0.15 }
+
+  validates :row,    :inclusion => { :in => 'A'..'H', :message => 'Row must be a letter between A and H'}
+  validates :column, :inclusion => { :in => 1..12, :message => 'Column must be an integer between 1 and 12'}
+
 end
--- spec/models/cnp_measurement_spec.rb
+++ spec/models/cnp_measurement_spec.rb
@@ -1,5 +1,93 @@
 require 'spec_helper'
 
 describe CnpMeasurement do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @cnp = CnpMeasurement.new
+    @cnp.leaf = Leaf.create(:code => 'L5', batch: Batch.new)
+    @cnp.row = 'G'
+    @cnp.column = 1
+    @cnp.c_percent = 48.9
+    @cnp.n_percent = 1.4
+    @cnp.cn_ratio  = 35.3
+    @cnp.n15_delta = -2.1
+    @cnp.n13_delta = -26.8
+    @cnp.cn_assays_file = 'CN_2013_PERU_1_RESULTS'
+    @cnp.p_corrected_avg = 0.1171
+    @cnp.p_corrected_var_coef = 0.0140
+    @cnp.p_assays_file = 'P_2013_Peru_REDO_1'
+    @cnp.box_number = 1
+    @cnp.batch = Batch.new
+  end
+
+  it 'is not valid on its own' do
+    expect(CnpMeasurement.new).to_not be_valid
+  end
+
+  it 'can be valid' do
+    expect(@cnp).to be_valid
+  end
+
+  it 'should check that deltas are negative' do
+    @cnp.n15_delta = 5
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:n15_delta)
+    @cnp.n13_delta = 101
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:n13_delta)
+  end
+
+  it 'should check that percentages are within 1 and 100' do
+    @cnp.c_percent = -1
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:c_percent)
+    @cnp.c_percent = 101
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:c_percent)
+    @cnp.n_percent = -1
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:n_percent)
+    @cnp.n_percent = 101
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:n_percent)
+    @cnp.cn_ratio = -1
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:cn_ratio)
+    @cnp.cn_ratio = 101
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:cn_ratio)
+  end
+
+  it 'should check that variation is not too high' do
+    @cnp.p_corrected_var_coef = 0.16
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:p_corrected_var_coef)
+  end
+
+  it 'should check that the row is a letter between A and H' do
+    @cnp.row = 0.16
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:row)
+    @cnp.row = 'I'
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:row)
+  end
+
+  it 'should check that the column is a number between 1 and 12' do
+    @cnp.column = 13
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:column)
+    @cnp.column = 'I'
+    expect(@cnp).to_not be_valid
+    expect(@cnp).to have(1).errors_on(:column)
+  end
+
+  it 'does not need percentages to be present' do
+    @cnp.c_percent = nil
+    @cnp.n_percent = nil
+    @cnp.cn_ratio  = nil
+    expect(@cnp).to be_valid
+  end
+
+
 end
