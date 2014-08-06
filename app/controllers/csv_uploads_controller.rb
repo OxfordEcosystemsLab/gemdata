bd3a15135b466e29bfd996477e6291afbc09e01c
Ben Holmes <bdvholmes@gmail.com>
2014-08-06 15:31:15 +0100

    Add CNP to import options

--- app/controllers/csv_uploads_controller.rb
+++ app/controllers/csv_uploads_controller.rb
@@ -24,6 +24,7 @@ class CsvUploadsController < ApplicationController
       BranchArchitectureImporter,
       ForestPlotsImporter,
       CnImporter,
+      CnpMeasurementImporter,
       ToughnessMeasurementImporter,
       WoodDensityImporter,
       SpectraMeasurementImporter,
