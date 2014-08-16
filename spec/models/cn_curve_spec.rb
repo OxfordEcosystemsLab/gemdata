557b53379569e678495f57f9a5c4bb0a160e3803
Ben Holmes <bdvholmes@gmail.com>
2014-08-16 14:49:09 +0100

    Add specs for curves

--- spec/models/cn_curve_spec.rb
+++ spec/models/cn_curve_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe CnCurve do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @cn_curve = CnCurve.new
+
+    [1, 2, 3, 4, 5, 6, 7, 8].each do |n|
+      @cn_curve.send("c_enr_#{n}=", 1)
+      @cn_curve.send("n_enr_#{n}=", 1)
+      @cn_curve.send("c_percent_#{n}=", 1)
+      @cn_curve.send("n_percent_#{n}=", 1)
+      @cn_curve.send("cn_ratio_#{n}=", 1)
+    end
+  end
+
+  it 'validates presence' do
+    @cn_curve.c_percent_4 = nil
+    expect(@cn_curve).to_not be_valid
+  end
+
+  it 'validates presence' do
+    @cn_curve.cn_ratio_8 = 'clifford the dog'
+    expect(@cn_curve).to_not be_valid
+  end
+
 end
--- spec/models/phosphorus_curve_spec.rb
+++ spec/models/phosphorus_curve_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe PhosphorusCurve do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before :each do
+    @p_curve = PhosphorusCurve.new
+
+    [0, 2, 4, 8, 12, 16].each do |n|
+      @p_curve.send("first_standard_#{n}=", 1)
+      @p_curve.send("second_standard_#{n}=", 1)
+      @p_curve.send("absorbance_#{n}=", 1)
+      @p_curve.send("concentration_#{n}=", 1)
+    end
+  end
+
+  it 'validates presence' do
+    @p_curve.first_standard_4 = nil
+    expect(@p_curve).to_not be_valid
+  end
+
+  it 'validates presence' do
+    @p_curve.absorbance_12 = 'clifford the dog'
+    expect(@p_curve).to_not be_valid
+  end
+
 end
