dd59e17910474c516f662fbdc3e0979f6c7ce0b9
Ben Holmes <bdvholmes@gmail.com>
2014-08-16 14:49:19 +0100

    Curves perform validation

--- app/models/cn_curve.rb
+++ app/models/cn_curve.rb
@@ -1,4 +1,26 @@
 class CnCurve < ActiveRecord::Base
   include BatchImport
 
+  [1, 2, 3, 4, 5].each do |n|
+    validates "c_enr_#{n}", presence: true
+    validates "n_enr_#{n}", presence: true
+    validates "c_percent_#{n}", presence: true
+    validates "n_percent_#{n}", presence: true
+    validates "cn_ratio_#{n}", presence: true
+
+    validates "c_enr_#{n}", numericality: true
+    validates "n_enr_#{n}", numericality: true
+    validates "c_percent_#{n}", numericality: true
+    validates "n_percent_#{n}", numericality: true
+    validates "cn_ratio_#{n}", numericality: true
+  end
+
+  [6, 7, 8].each do |n|
+    validates "c_enr_#{n}", presence: true
+    validates "n_enr_#{n}", presence: true
+
+    validates "c_enr_#{n}", numericality: true
+    validates "n_enr_#{n}", numericality: true
+  end
+
 end
--- app/models/phosphorus_curve.rb
+++ app/models/phosphorus_curve.rb
@@ -1,4 +1,16 @@
 class PhosphorusCurve < ActiveRecord::Base
   include BatchImport
 
+  [0, 2, 4, 8, 12, 16].each do |n|
+    validates "first_standard_#{n}", presence: true
+    validates "second_standard_#{n}", presence: true
+    validates "absorbance_#{n}", presence: true
+    validates "concentration_#{n}", presence: true
+
+    validates "first_standard_#{n}", numericality: true
+    validates "second_standard_#{n}", numericality: true
+    validates "absorbance_#{n}", numericality: true
+    validates "concentration_#{n}", numericality: true
+  end
+
 end
