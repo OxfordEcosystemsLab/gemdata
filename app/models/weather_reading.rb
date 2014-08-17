10fafc8fe3e9b3c0d1d1ad6ceca3224e616ebd08
Ben Holmes <bdvholmes@gmail.com>
2014-08-17 13:40:33 +0100

    Weather reading raises exception

--- app/models/weather_reading.rb
+++ app/models/weather_reading.rb
@@ -7,12 +7,12 @@ class WeatherReading < ActiveRecord::Base
   validates :datetime, :presence => true
 
   def self.find_between(this, that)
-    self.readings_between(this, that).first
+    self.between(this, that).first!
   end
 
   private
 
-  def self.readings_between(this, that)
+  def self.between(this, that)
     self.where(:datetime => this..that)
   end
 
