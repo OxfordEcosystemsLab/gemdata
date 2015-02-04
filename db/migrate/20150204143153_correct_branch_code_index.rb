8e28c9045feb849d486c86ca70f54bf9419ebf00
Andy Preston <andy.preston@inzent.co.uk>
2015-02-04 17:52:51 +0000

    Migration branch code index is no longer unique

---
+++ db/migrate/20150204143153_correct_branch_code_index.rb
@@ -0,0 +1,11 @@
+class CorrectBranchCodeIndex < ActiveRecord::Migration
+  def change
+    # This index was originally unique
+    # Which resulted in it only being possible to have ONE branch 'B1S'
+    # in the entire table
+    #
+    # see 20140322201843_create_branches.rb
+    remove_index :branches, :code
+    add_index :branches, :code
+  end
+end
