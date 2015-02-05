fc9ae6a4ccfa1f7dcb3947975b3fd6d03c8f4f64
Andy Preston <andy.preston@inzent.co.uk>
2015-02-05 11:45:53 +0000

    modified code reader to come in line with changes to requirements

--- app/classes/import/code_reader.rb
+++ app/classes/import/code_reader.rb
@@ -5,8 +5,8 @@ class CodeReader
   attr_reader :plot_code, :tree_code, :branch_code, :leaf_code, :suffix
 
   def initialize(code)
-    #format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+\w?)(?:-(.+))?)?)?$/
-    format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
+    format = /^(\w+\d+)-((?:CSP\d+-\d+|\w[\.\d]+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
+    #format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
     match = code.match(format)
     if (match)
       @plot_code   = match[1].upcase
