2d7373f454ee573c720f6c20735412cc4dd95f3a
Andy Preston <edgeeffect@gmail.com>
2015-02-06 16:20:35 +0000

    plot codes can now have a dash in them and will be treated the same as plot codes with no dash

--- app/classes/import/code_reader.rb
+++ app/classes/import/code_reader.rb
@@ -5,11 +5,10 @@ class CodeReader
   attr_reader :plot_code, :tree_code, :branch_code, :leaf_code, :suffix
 
   def initialize(code)
-    format = /^(\w+\d+)-((?:CSP\d+-\d+|\w[\.\d]+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
-    #format = /^(\w+\d+)-((?:CSP\d+-\d+|\w\d+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
+    format = /^(\w+-?\d+)-((?:CSP\d+-\d+|\w[\.\d]+))(?:[-\s](\w+\d*\w*)(?:-(\w\d+)(?:(\w\d+))?)?)?$/
     match = code.match(format)
     if (match)
-      @plot_code   = match[1].upcase
+      @plot_code   = match[1].upcase.gsub('-','')
       @tree_code   = match[2]
       @branch_code = match[3]
       @leaf_code   = match[4]
