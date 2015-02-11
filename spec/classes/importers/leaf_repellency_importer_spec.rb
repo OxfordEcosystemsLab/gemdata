03b6a77f004cfc1b3dc12fd08bc96018e21a4535
Andy Preston <edgeeffect@gmail.com>
2015-02-11 16:30:32 +0000

    fixed tests for leaf repellency importer

--- spec/classes/importers/leaf_repellency_importer_spec.rb
+++ spec/classes/importers/leaf_repellency_importer_spec.rb
@@ -25,4 +25,20 @@ describe LeafRepellencyImporter do
     expect(lr).to be_valid
   end
 
+  it 'can read CSV with a space in the plot code' do
+    values = CSV.parse_line 'ACJ,ACJ 01-T733-B3S,L1,5237,'
+
+    importer = LeafRepellencyImporter.new(1, 2)
+    status = importer.read_row(values, Array.new)
+    expect(status).to eq(Lookup::ImportStatus.inserted)
+
+    lr = importer.object.reload
+    expect(lr.branch).to eq(@branch)
+    expect(lr.replica).to eq('L1')
+    expect(lr.photo_number).to eq(5237)
+    expect(lr.angle).to be_nil
+
+    expect(lr).to be_valid
+  end
+
 end
