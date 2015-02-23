0452e18abf7bb1cea5bc6f137e64a997c5de8103
Andy Preston <edgeeffect@gmail.com>
2015-02-23 17:23:48 +0000

    Fixed constraints in carnegie chemistry

--- app/models/carnegie_bulk_leaf_chemistry.rb
+++ app/models/carnegie_bulk_leaf_chemistry.rb
@@ -16,24 +16,24 @@ class CarnegieBulkLeafChemistry < ActiveRecord::Base
   # flagged - indicating a chemical value was removed
   # reason - explaining which value was removed
   validates :date_collected, presence: true
-  validates :n, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :n, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
   # chl_a - mg g^(-1) dry mass
   # chl_b - mg g^(-1) dry mass
   # carotenoids - mg g^(-1) dry mass
-  validates :soluble_c, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :soluble_c, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
   # delta_13c - per mill dry mass
   validates :water, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
-  validates :p, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :ca, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :k, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
+  validates :p, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
+  validates :ca, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
+  validates :k, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
   validates :mg, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
   # lma - g dry m^(-2) leaf
   validates :c, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
-  validates :lignin, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :cellulose, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :hemi_cellulose, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
-  validates :phenols, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }
-  validates :tannins, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }
+  validates :lignin, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
+  validates :cellulose, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
+  validates :hemi_cellulose, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }, :allow_nil => true
+  validates :phenols, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }, :allow_nil => true
+  validates :tannins, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 1000 }, :allow_nil => true
   
   # b - mg g^(-1) dry mass
   # fe - mg g^(-1) dry mass
