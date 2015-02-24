22ebda216e467331cf3537fec7bbf83b034c12a2
Andy Preston <edgeeffect@gmail.com>
2015-02-24 14:42:40 +0000

    Fixed tests for light reference importer

--- spec/classes/importers/light_reference_importer_spec.rb
+++ spec/classes/importers/light_reference_importer_spec.rb
@@ -19,7 +19,7 @@ describe LightReferenceImporter do
     expect(lr.datetime).to eq(Time.parse('2013-05-21 08:00:00'))
     expect(lr.number).to eq(1)
     expect(lr.measurement).to eq(1.00305)
-    expect(lr.photons).to eq(896.0366016)
+    expect(lr.ppfd).to eq(896.0366016)
   end
 
   it 'performs validation' do
