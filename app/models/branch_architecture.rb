d0a09b6573213f73d49ceb4914d83b64e6a77813
Andy Preston <edgeeffect@gmail.com>
2015-02-25 16:42:54 +0000

    fixed branch arhitecture validation

--- app/models/branch_architecture.rb
+++ app/models/branch_architecture.rb
@@ -4,17 +4,25 @@ class BranchArchitecture < ActiveRecord::Base
   include BatchImport
 
   belongs_to :branch
-
   validates :branch,    presence: true
+  
   validates :section,   presence: true
   validates :parent,    presence: true
   validates :tip,       presence: true
 
-  validates_each :diam_inf, :diam_sup, :longitude do |record, attr, value|
-    if (record.tip == 'N' && value)
+  validates_each :diam_inf do |record, attr, value|
+    if (record.tip == 'Node' and value)
       record.errors.add(attr, 'cannot be defined for "Node" tips')
-    elsif (record.tip != 'N' && value.nil?)
-      record.errors.add(attr, 'must be defined if not a "Node" tip')
+    elsif (record.tip =~ /^[F|C|T]$/ and value.nil?)
+      record.errors.add(attr, 'must be defined for a "F", "C" or "T" tip')
+    end
+  end
+
+  validates_each :diam_sup, :longitude do |record, attr, value|
+    if (record.tip == 'Node' and value)
+      record.errors.add(attr, 'cannot be defined for "Node" tips')
+    elsif (record.tip =~ /^[F|T]$/ and value.nil?)
+      record.errors.add(attr, 'must be defined for a "F" or "T" tip')
     end
   end
 
