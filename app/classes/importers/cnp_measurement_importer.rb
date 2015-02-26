9374de588cb9ba902ef10fc451f667a73a2aeaca
Andy Preston <edgeeffect@gmail.com>
2015-02-26 11:56:07 +0000

    fixed CNP validation

--- app/classes/importers/cnp_measurement_importer.rb
+++ app/classes/importers/cnp_measurement_importer.rb
@@ -17,22 +17,22 @@ class CnpMeasurementImporter < RowImporter
     # 6 Site
     # 7 Row in tray
     # 8 Column in tray
-    @cnp.c_percent = values[9]
+    @cnp.c_percent = values[9] unless values[9] == 'NA'
     @cnp.c_percent_quality = values[10]
-    @cnp.n_percent = values[11]
+    @cnp.n_percent = values[11] unless values[11] == 'NA'
     @cnp.n_percent_quality = values[12]
-    @cnp.cn_ratio  = values[13]
+    @cnp.cn_ratio  = values[13] unless values[13] == 'NA'
     @cnp.cn_ration_quality = values[14]
-    @cnp.n15_delta = values[15]
+    @cnp.n15_delta = values[15] unless values[15] == 'NA'
     @cnp.n15_delta_quality = values[16]
-    @cnp.c13_delta = values[17]
+    @cnp.c13_delta = values[17] unless values[17] == 'NA'
     @cnp.c13_delta_quality = values[18]
     @cnp.cn_assays_file = values[19]
     # 20 Site
     @cnp.original_P_leafcode = values[21]
-    @cnp.p_corrected_percent = values[22]
-    @cnp.p_corrected_percent_cv = values[23]
-    @cnp.p_corrected_percent_quality = values[24]
+    @cnp.p_corrected_percent = values[22] unless values[22] == 'NA'
+    @cnp.p_corrected_percent_cv = values[23] unless values[23] == 'NA'
+    @cnp.p_corrected_percent_quality = values[24] unless values[24] == 'NA'
     @cnp.p_assays_file = values[25]
     # 26 box number for P sample
     save_with_status!
--- app/models/cnp_measurement.rb
+++ app/models/cnp_measurement.rb
@@ -5,8 +5,9 @@ class CnpMeasurement < ActiveRecord::Base
   validates :leaf_part, :presence => true
 
   validates :n_percent, :c_percent, :cn_ratio, :p_corrected_percent, :numericality => { :greater_than_or_equal_to => 0, :less_than_or_equal_to => 100 }, :allow_nil => true
-  validates :n15_delta, :c13_delta, :numericality => { :less_than => 0 }
-  validates :p_corrected_percent_cv,  :numericality => { :less_than => 0.15 }
+  validates :n15_delta, :presence => true, :allow_nil => true
+  validates :c13_delta, :numericality => { :less_than => 0 }, :allow_nil => true
+  validates :p_corrected_percent_cv,  :numericality => { :less_than => 0.15 }, :allow_nil => true
 
 end
 
