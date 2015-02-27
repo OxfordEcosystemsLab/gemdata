307c5d6b1504803f7ae6eb781b5e7db4dc1917bd
Andy Preston <edgeeffect@gmail.com>
2015-02-27 10:36:38 +0000

    fix issues with SLA and "NA" data

--- app/classes/importers/specific_leaf_area_importer.rb
+++ app/classes/importers/specific_leaf_area_importer.rb
@@ -9,15 +9,15 @@ class SpecificLeafAreaImporter < RowImporter
       :leaf => find_or_create_leaf(values[0])
     })
     attempt_to_overwrite!(@sla)
-    @sla.lamina_area = values[2]
+    @sla.lamina_area = values[2] unless values[2] == 'NA'
     @sla.lamina_drymass = values[3] unless values[3] == 'NA'
     @sla.laminapetiole_area = values[4] unless values[4] == 'NA'
     @sla.laminapetiole_drymass = values[5] unless values[5] == 'NA'
     @sla.comment = values[6]
     @sla.quality_flag = values[7]
-    @sla.sla_lamina = values[8]
+    @sla.sla_lamina = values[8] unless values[8] == 'NA'
     @sla.sla_lamina_petiole = values[9] unless values[9] == 'NA'
-    @sla.lma_lamina = values[10]
+    @sla.lma_lamina = values[10] unless values[10] == 'NA'
     @sla.lma_lamina_petiole = values[11] unless values[11] == 'NA'
     save_with_status!
   end
