96a6e6c900eb864ee4c08484b610c4e9cae54223
ashenkin <github@shenkin.org>
2016-06-02 14:43:30 +0100

    keep misc sql queries here.  this one is for blowing away a bad forestplots import, assuming no other data has been linked to it after the initial import.

---
+++ db/misc_sql/delete_fp_import.sql
@@ -0,0 +1,14 @@
+BEGIN;
+
+DELETE from dbh_measurements d USING trees t, sub_plots sp, plots p
+WHERE  d.tree_id = t.id 
+AND t.sub_plot_id = sp.id
+AND  sp.plot_id = p.id
+AND p.plot_code = 'INSERT_PLOTCODE_HERE';
+
+DELETE from trees t USING sub_plots sp, plots p
+WHERE t.sub_plot_id = sp.id
+AND  sp.plot_id = p.id
+AND p.plot_code = 'INSERT_PLOTCODE_HERE';
+
+COMMIT;
